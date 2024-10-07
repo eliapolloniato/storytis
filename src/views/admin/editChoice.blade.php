4e582b5e5bed55144d62e512cdf9fd9bda5124fb
asasamo <elia@eliapolloniato.com>
2024-10-07 19:33:16 +0200

    Aggiunte ricompense

--- src/views/admin/editChoice.blade.php
+++ src/views/admin/editChoice.blade.php
@@ -74,6 +74,41 @@
                     </select>
                 </div>
 
+                <div class="my-5">
+                    <label for="reward"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ricompensa</label>
+                    <div class="flex">
+                        <a class="cursor-pointer flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-500 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+                            href="{{ $config['routes']['reward'] . 'add' }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"
+                                class="size-5 fill-primary-500">
+                                <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+                            </svg>
+                        </a>
+                        <select id="reward"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-e-lg border-s-gray-100 dark:border-s-gray-700 border-s-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            @isset($choice)
+                                @if ($choice->getReward() !== null)
+                                    <option value="{{ $choice->getReward()->getId() }}" selected>
+                                        {{ $choice->getReward()->getDescription() }}</option>
+                                @endif
+                                @foreach (array_filter(Reward::getAll(), static fn($e) => $e->getId() != ($choice->getReward() !== null ? $choice->getReward()->getId() : null)) as $reward)
+                                    <option value="{{ $reward->getId() }}">
+                                        {{ '(' . $reward->getAffectedSkillName() . ', ' . $reward->getValue() . ') - ' . $reward->getDescription() }}
+                                    </option>
+                                @endforeach
+                            @else
+                                @foreach (Reward::getAll() as $reward)
+                                    <option value="{{ $reward->getId() }}">
+                                        {{ '(' . $reward->getAffectedSkillName() . ', ' . $reward->getValue() . ') - ' . $reward->getDescription() }}
+                                    </option>
+                                @endforeach
+
+                            @endisset
+                        </select>
+                    </div>
+                </div>
+
                 <button type="submit"
                     class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                     Salva
