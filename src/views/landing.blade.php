7cfe0867f6fe1a60db23fb305d15d0af2984bedd
asasamo <elia@eliapolloniato.com>
2024-10-07 19:33:16 +0200

    Aggiunta landing page

---
+++ src/views/landing.blade.php
@@ -0,0 +1,4 @@
+<div class="p-5 w-full h-full max-w-screen-xl mx-auto">
+    <h1 class="text-6xl text-center uppercase font-thin">Storytis</h1>
+    <p class="text-center text-gray-500 text-xl font-thin">Vivi ed esplora avventure incredibili</p>
+</div>
