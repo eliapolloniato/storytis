d8d5fd7bf9aab775b9318b7b08fbe9d2bff00f22
asasamo <elia@eliapolloniato.com>
2024-10-07 19:34:43 +0200

    Aggiunto logo nella landing page

--- src/views/landing.blade.php
+++ src/views/landing.blade.php
@@ -1,4 +1,5 @@
 <div class="p-5 w-full h-full max-w-screen-xl mx-auto">
+    <img src="/static/img/logo.svg" alt="Storytis" class="mx-auto my-10 size-72 drop-shadow-logo">
     <h1 class="text-6xl text-center uppercase font-thin">Storytis</h1>
     <p class="text-center text-gray-500 text-xl font-thin">Vivi ed esplora avventure incredibili</p>
 </div>
