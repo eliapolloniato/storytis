d5981daff9eb62b006ef950d7af83156df69373a
asasamo <elia@eliapolloniato.com>
2024-10-07 19:33:15 +0200

    Primo controllo sull'autenticazione dell'utente

--- src/routes/login.php
+++ src/routes/login.php
@@ -1,4 +1,8 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once __DIR__ . "/../router.php";
 require_once __DIR__ . "/../utils/loadPage.php";
 $blade = require __DIR__ . "/../utils/blade.php";
@@ -8,7 +12,45 @@ $router->mount("/login", function () use ($router, $blade) {
         echo loadPage($blade->render("login", ["action" => "login"]), "Login");
     });
 
+    $router->post("/", function () use ($blade) {
+        if (!isset($_POST["action"])) {
+            header("Location: /login");
+            exit();
+        }
+
+        if ($_POST["action"] === "login") {
+            // LOGIN
+            if (isset($_POST["username"], $_POST["password"])) {
+                $username = $_POST["username"];
+                $password = $_POST["password"];
+
+                // TODO: check if the user exists and the password is correct
+                $user = null;
+
+                $_SESSION["user"] = $user;
+            }
+        } else if ($_POST["action"] === "register") {
+            // REGISTER
+            if (isset($_POST["username"], $_POST["password"])) {
+                $username = $_POST["username"];
+                $password = $_POST["password"];
+
+                // TODO: create user
+                $user = null;
+
+                $_SESSION["user"] = $user;
+            }
+        }
+    });
+
     $router->get("/register", function () use ($blade) {
         echo loadPage($blade->render("login", ["action" => "register"]), "Registrati");
+    });
+
+    $router->get("/logout", function () use ($blade) {
+        // Destroy the session and redirect to the home page
+        session_destroy();
+        header("Location: /home");
+        exit();
     });
 });
--- src/routes/story.php
+++ src/routes/story.php
@@ -7,14 +7,14 @@ require_once __DIR__ . "/../router.php";
 require_once __DIR__ . "/../utils/loadPage.php";
 $blade = require __DIR__ . "/../utils/blade.php";
 
-$router->mount("/home", function () use ($router, $blade) {
-    $router->get("/", function () use ($blade) {
-        echo loadPage($blade->render("home", ["text" => "Home"]), "Home");
+$router->mount("/story", function () use ($router, $blade) {
+    $router->get("/play/(\d+)", function ($storyId) use ($blade) {
+        echo "Story $storyId";
     });
 });
 
-// Check if the user is logged in before accessing the home subroutes
-$router->before("GET|POST", "/home.*", function () {
+// Check if the user is logged in before accessing the story subroutes
+$router->before("GET|POST", "/story.*", function () {
     if (!isset($_SESSION["user"])) {
         header("Location: /login");
         exit();
