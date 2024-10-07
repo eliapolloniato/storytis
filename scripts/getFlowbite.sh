6da76c7024bcabfb49c91795c0145e0cdb037d1c
asasamo <elia@eliapolloniato.com>
2024-10-07 19:33:15 +0200

    Aggiornato script per flowbite

--- scripts/getFlowbite.sh
+++ scripts/getFlowbite.sh
@@ -1,2 +1,3 @@
+mkdir -p src/dist
 wget https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js -O src/dist/flowbite.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css -O src/dist/flowbite.min.css
