fa3f0ccf235c5ccf6f46975fc4a725ca7ec5124f
asasamo <elia@eliapolloniato.com>
2024-10-07 19:33:15 +0200

    Aggiornato css di flowbite

--- getFlowbite.sh
+++ getFlowbite.sh
@@ -1,1 +1,2 @@
 wget https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js -O src/dist/flowbite.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css -O src/dist/flowbite.min.css
