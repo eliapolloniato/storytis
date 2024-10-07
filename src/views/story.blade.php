b3335eed0e3f771ebd598c94fd3261c9a5566a2b
asasamo <elia@eliapolloniato.com>
2024-10-07 19:33:16 +0200

    Aggiunto modal per modifica caratteristiche

--- src/views/story.blade.php
+++ src/views/story.blade.php
@@ -87,7 +87,7 @@
                             </div>
                         </div>
                     </div>
-                    <button type="button"
+                    <button type="button" data-modal-target="character-edit" data-modal-toggle="character-edit"
                         class="py-1.5 px-3 m-1 text-sm font-medium w-full text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-primary-400 dark:hover:bg-gray-700">
                         Modifica
                     </button>
@@ -96,6 +96,63 @@
             <div class="p-2 w-1/2 md:w-full md:h-4/6 z-10">
                 <div id="inventory"
                     class="p-2 h-full w-full border-2 border-gray-200 dark:border-gray-800 rounded-lg shadow-lg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Character edit modal -->
+<div id="character-edit" tabindex="-1" aria-hidden="true"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Modifica caratteristiche del personaggio
+                </h3>
+                <button type="button"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="character-edit">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Chiudi</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+
+            <div class="p-4 md:p-5 space-y-4">
+
+                <h2 class="text-xl">Punti disponibili: <span class="text-primary-500"
+                        id="available-points">{{ '2' }}</span>
+                </h2>
+
+                @include('components.inputCounter', [
+                    'name' => 'Forza',
+                    'min' => 0,
+                    'max' => 10,
+                    'value' => 5,
+                ])
+
+                @include('components.inputCounter', [
+                    'name' => 'Intelligenza',
+                    'min' => 0,
+                    'max' => 15,
+                    'value' => 5,
+                ])
+
+                <script src="/static/script/editCharacter.js" defer></script>
+                <!-- Modal footer -->
+                <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button data-modal-hide="character-edit" type="button"
+                        class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Salva</button>
+                    <button data-modal-hide="character-edit" type="button"
+                        class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Annulla</button>
                 </div>
             </div>
         </div>
