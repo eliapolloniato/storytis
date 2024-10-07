27fd845d0d2abfce4324836c433ad0717c2b4d32
asasamo <elia@eliapolloniato.com>
2024-10-07 19:33:15 +0200

    Aggiornati gli z index

--- src/views/story.blade.php
+++ src/views/story.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full h-full p-5">
-    <div class="w-full h-full max-w-screen-xl flex flex-col md:flex-row mx-auto">
-        <div class="p-2 h-3/5 md:h-full md:w-1/2">
+    <div class="w-full h-full max-w-screen-xl flex flex-col md:flex-row mx-auto z-0">
+        <div class="p-2 h-3/5 md:h-full md:w-1/2 z-10">
             <div id="story-content"
                 class="p-2 h-full w-full flex flex-col justify-stretch border-2 border-gray-200 dark:border-gray-800 rounded-lg shadow-lg">
                 <h1
@@ -14,31 +14,31 @@
                     <!-- MAX 4 BUTTONS -->
                     <button type="button"
                         class="py-2.5 px-5 m-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-primary-400 dark:hover:bg-gray-700">
-                        {{ 'Scelta 1' }}
+                        {{ '1' }}
                     </button>
                     <button type="button"
                         class="py-2.5 px-5 m-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-primary-400 dark:hover:bg-gray-700">
-                        {{ 'Scelta 2' }}
+                        {{ '2' }}
                     </button>
                     <button type="button"
                         class="py-2.5 px-5 m-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-primary-400 dark:hover:bg-gray-700">
-                        {{ 'Scelta 3' }}
+                        {{ '3' }}
                     </button>
                     <button type="button"
                         class="py-2.5 px-5 m-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-primary-400 dark:hover:bg-gray-700">
-                        {{ 'Scelta 4' }}
+                        {{ '4' }}
                     </button>
 
                 </div>
             </div>
         </div>
         <div class="flex flex-row h-2/5 md:h-full md:flex-col md:w-1/2 ">
-            <div class="p-2 w-1/2 md:w-full md:h-2/6">
+            <div class="p-2 w-1/2 md:w-full md:h-2/6 z-10">
                 <div id="character-info"
                     class="p-2 h-full w-full border-2 border-gray-200 dark:border-gray-800 rounded-lg shadow-lg">
                 </div>
             </div>
-            <div class="p-2 w-1/2 md:w-full md:h-4/6">
+            <div class="p-2 w-1/2 md:w-full md:h-4/6 z-10">
                 <div id="inventory"
                     class="p-2 h-full w-full border-2 border-gray-200 dark:border-gray-800 rounded-lg shadow-lg">
                 </div>
